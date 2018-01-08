58cb9ff6253f181fd69fe758e99cb08b768cef41
andrewklajman <andrew.klajman@gmail.com>
2018-01-08 21:11:34 +1100

    Add files via upload

--- Checkers.py
+++ Checkers.py
@@ -6,54 +6,68 @@ CROSS = 'X'
 INVALID_MOVE = 'INVALID MOVE'
 BOARD = []
 
+currentPlayer = NAUGHT
+
 def printBoard():
     print('  ' + ''.join([str(i) for i in range(BOARD_SIZE)]))
     for r in range(BOARD_SIZE):
         print(str(r) + ' ' + ''.join(BOARD[r]))
     print()
 
-def movePiece(curPos, proPos):
-    if BOARD[curPos[0]][curPos[1]] == NAUGHT:
-        playerPerspective = 1
-    elif BOARD[curPos[0]][curPos[1]] == CROSS:
-        playerPerspective = -1
-    else:
-        return INVALID_MOVE
-
-    if ((curPos[0] - proPos[0]) == 1 or (curPos[0] - proPos[0]) == - 1) and (curPos[1] - proPos[1]) == 1 * playerPerspective and BOARD[proPos[0]][proPos[1]] == BLANK:
-        BOARD[curPos[0]][curPos[1]] = BLANK
-        if playerPerspective == 1:
-            BOARD[proPos[0]][proPos[1]] = NAUGHT
-        elif playerPerspective == -1:
-            BOARD[proPos[0]][proPos[1]] = CROSS
-
-    elif ((curPos[0] - proPos[0]) == 2 or (curPos[0] - proPos[0]) == - 2) and (curPos[1] - proPos[1]) == -2 * playerPerspective:
-        BOARD[curPos[0]][curPos[1]] = BLANK
-        if playerPerspective == 1:
-            BOARD[proPos[0]][proPos[1]] = NAUGHT
-        elif playerPerspective == -1:
-            BOARD[proPos[0]][proPos[1]] = CROSS
-
-
-
-
-
-
-            
-    
-for r in range(BOARD_SIZE):
+def potentialMoves(CurR,CurC):
     tmp = []
-    for c in range(BOARD_SIZE):
-            tmp.append(BLANK)
-    BOARD.append(tmp)
 
+    if BOARD[CurR - 1 * orient][CurC - 1] == BLANK:
+        tmp.append((CurR - 1 * orient,CurC - 1))
+    if BOARD[CurR - 1 * orient][CurC + 1] == BLANK:
+        tmp.append((CurR - 1 * orient,CurC + 1))
+
+    if 0 <= CurR - 1 * orient < BOARD_SIZE:
+        if 0 <= CurC + 2 < BOARD_SIZE:
+            if BOARD[CurR - 2 * orient][CurC + 2] == BLANK and BOARD[CurR - 1 * orient][CurC + 1] == opposingPlayer(currentPlayer):
+                tmp.append((CurR - 2 * orient,CurC + 2))
+        if 0 <= CurC - 2 < BOARD_SIZE:
+            if BOARD[CurR - 2 * orient][CurC - 2] == BLANK and BOARD[CurR - 1 * orient][CurC - 1] == opposingPlayer(currentPlayer):
+                tmp.append((CurR - 2 * orient,CurC - 2))
+
+    return tmp
+
+def opposingPlayer(player):
+    if player == NAUGHT:
+        return CROSS
+    return NAUGHT
+
+    
+
+def initBoard():
+    for r in range(BOARD_SIZE):
+        tmp = []
+        for c in range(BOARD_SIZE):
+                tmp.append(BLANK)
+        BOARD.append(tmp)
+
+
+initBoard()
 BOARD[4][4] = NAUGHT
+BOARD[4][6] = NAUGHT
 BOARD[3][5] = CROSS
 
+
+if currentPlayer == CROSS:
+    orient = -1
+else:
+    orient = 1
+
+cpos = (4,6)
+ppos = (2,4)
+print(ppos in potentialMoves(4,6))
+
+if abs(ppos[0] - cpos[0]) == 2:
+    BOARD[cpos[0] - 1 * orient][abs(cpos[1] - ppos[1] + 1)] = BLANK
+    print
+
+
 printBoard()
 
-##movePiece((3,5),(4,6))
-##movePiece((4,4),(3,3))
-movePiece((4,4),(2,6))
 
-printBoard()
+ 
